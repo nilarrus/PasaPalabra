2160154e8cd9bcb346d4f53586baa2476c3c703f
Nilarrus <nilarrus69@gmail.com>
2019-05-09 18:03:45 +0200

    rosco puesto

--- pasapalabra/routes/web.php
+++ pasapalabra/routes/web.php
@@ -1,20 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*Route::get('/', function () {
-    return view('welcome');
-});
-*/
 Auth::routes();
-
+//Pagina Login/inicial
 Route::get('/', 'HomeController@index')->name('home');
+
+//Pagina Level
+Route::get('/level','LevelController@index')->name('Level_index');
+
+//Pagina Ranking
+Route::get('/ranking','RangkingController@index')->name('Ranking_index');
+
+//Pagina Game 
+Route::get('/game','GameController@index')->name('Game_index');
