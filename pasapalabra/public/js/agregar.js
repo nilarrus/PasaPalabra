f99fe14abe439b41630a817f7d45f77057f35198
Nilarrus <nilarrus69@gmail.com>
2019-05-15 19:06:47 +0200

    arreglos

--- pasapalabra/public/js/agregar.js
+++ pasapalabra/public/js/agregar.js
@@ -1,7 +1,7 @@
 $("#agregaS").click(function() {
 	var data=$("#palabra").val();
 	var route="http://localhost:8000/agregar";
-	var token=$("#token"),val();
+	var token=$("#token").val();
 	$.ajax({
 		url:route,
 		headers:{'X-CSRF-TOKEN':token},
