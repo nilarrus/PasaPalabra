241a5761c5878bd893f3e33640b29d3a2675b1ea
root <super>
2019-05-24 19:47:36 +0200

    lg

--- pasapalabra/resources/views/auth/login.blade.php
+++ pasapalabra/resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
 
                         
 
-                         <div class="form-group row mb-0">
+                         <div class="form-group row mb-12">
                             <div class="col-md-6 offset-md-4">
                             
                                 <button type="submit" class="btn btn-lg btn-primary btn-block text-uppercase" >
